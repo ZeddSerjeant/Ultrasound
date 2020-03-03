de6e06de5ca3a21c67e5e8a5f25ecddf70286ef3
Zedd Serjeant <zjtp1@students.waikato.ac.nz>
2020-03-03 13:47:48 +1300

    Timing is as accurate as possible, a spreadsheet for calculations has been included

--- Ultrasound.X/main.c
+++ Ultrasound.X/main.c
@@ -18,11 +18,16 @@
 
 unsigned char led_state = OFF; // for ease of toggling in an interrupt
 
+unsigned char timer0_initial= 0; // for timing smaller than a single time loop.
+unsigned char timer0_delay = 0; // for times longer than the timer itself
+unsigned char delay_count = 0; // for use with the above
+
 void interrupt ISR()
 {
 	if (TIMER0_INTERRUPT_FLAG) // if the timer0 interrupt flag was set (timer0 triggered)
 	{
-		led_state = ~led_state; // toggle LED
+		delay_count++; // increment time delay
+		TIMER0_COUNTER = timer0_initial + 2; // reset counter, but also add 2 since it takes 2 clock cycles to get going
 		TIMER0_INTERRUPT_FLAG = 0; // clear interrupt flag since we dealt with it
 	}
 }
@@ -31,9 +36,14 @@ void main()
 {
     // TRISIO = 0xDF; // make GP5 an output using full byte (faster to set multiple things)
 	LED_PIN = 0; // Set GP5 to output directly. Slower with more outputs, but more readable
-    LED = OFF; // Initalize LED as off
+    LED = led_state; // Initalize LED
+
+    //calculate intial for accurate timing $ inital = TimerMax-((Delay*Fosc)/(Prescaler*4))
+    // This shrinks timing smaller than directly prescaling for when thats necessary
+    timer0_initial = 17;
+    timer0_delay = 10; // for 500ms and prescaler of 1:256 (adjusted empirically)
     
-   	TIMER0_COUNTER = CLEAR; // clear counter
+   	TIMER0_COUNTER = timer0_initial; // set counter
    	TIMER0_CLOCK_SCOURCE = TIMER_ENABLE; // internal clock
    	PRESCALER = 0; // enable prescaler for Timer0
    	PS2=1; PS1=1; PS0=1; // Set prescaler to 1:256
@@ -42,7 +52,11 @@ void main()
 
     while (1)
     {
-   		LED = led_state;
+    	if (delay_count >= timer0_delay)
+   		{
+   			LED = ~LED;
+   			delay_count = 0;
+   		}
     }
     return;
 }
