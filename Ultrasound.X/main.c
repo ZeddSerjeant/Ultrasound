abd38a5715e341415b5de0f6ea48648fb73e2422
Zedd Serjeant <zjtp1@students.waikato.ac.nz>
2020-03-18 17:11:21 +1300

    broken mostly, but changes to led duty cycle is now fully encapsulated in the interrupt

--- Ultrasound.X/main.c
+++ Ultrasound.X/main.c
@@ -17,14 +17,18 @@ unsigned int led_duty_cycle = 0; // Duty cycle of LED as on_time[ms]
 const unsigned int led_period = 1000; // period of flashing LED [ms]
 unsigned int led_duty_cycle_counter = 0;
 
-const unsigned int ping_delay = 20; //[ms]
+//[ms]
+#define PING_DELAY 20
+unsigned int ping_delay_count;
 
-unsigned char timer0_initial= 0; // for timing smaller than a single time loop.
-unsigned char timer0_delay = 0; // for times longer than the timer itself
-unsigned char delay_count = 0; // for use with the above
+// for timing smaller than a single time loop.
+#define TIMER0_INITIAL 118
+// unsigned char timer0_delay = 0; // for times longer than the timer itself
+// unsigned char delay_count = 0; // for use with the above
 
 bit device_state = 0; // pressing the button alters state
-const unsigned char button_bounce = 200; // a number of interrupts to allow ignoring button bounce [ms]
+// a number of interrupts to allow ignoring button bounce [ms]
+#define BUTTON_BOUNCE 200
 unsigned char button_bounce_count = 0; // to increment to this value
 
 bit do_calcs = 0; // for the ping cycle to indicate it has captured something so we can make calculations
@@ -38,8 +42,9 @@ union time // a union is used for ease of adjusting the range whiles assigning t
 		unsigned char high_byte;
 	};
 } range_to_target; // this represents the value to initialise timer1 to, so that it counts down approximately the value after the subtraction. This is the range as this is increased to search further away, and will represent the time/distance to the object when it is found
-const unsigned char range_band = 20; // [us] to begin with, I'm doing a linear search which will proceed in steps of this
-const unsigned short int initial_range = 0xFFFF - 198; // [us] ~33mm from beginning the range from the transducer to begin searching for objects
+const unsigned char initial_range_band = 20; // [us] to begin with, I'm doing a linear search which will proceed in steps of this
+unsigned short int range_band = 20;
+const unsigned char initial_range = 0xFFFF - 198; // [us] ~33mm from beginning the range from the transducer to begin searching for objects
 
 // const unsigned short int min_range = 0xFFFF - 343; // 10cm round trip, so 5cm from beginning
 // const unsigned short int max_range = 0xFFFF - 488; // 16cm, 8cm from beginning
@@ -49,6 +54,7 @@ const unsigned short int max_range = 0xFFFF - 3499; // [us] the max range, about
 unsigned short int read_threshold = 0; // XXX the threshold for the previous variable
 unsigned short int receiver_dc_offset = 0; // set on calibration
 
+unsigned long int magnitude1;
 unsigned char readings[2]; // an array for storing ADC results
 
 
@@ -58,9 +64,45 @@ void interrupt ISR()
 	if (TIMER0_INTERRUPT_FLAG) // if the timer0 interrupt flag was set (timer0 triggered)
 	{
 		TIMER0_INTERRUPT_FLAG = CLEAR; // clear interrupt flag since we are dealing with it
-		TIMER0_COUNTER = timer0_initial + 2; // reset counter, but also add 2 since it takes 2 clock cycles to get going
+		TIMER0_COUNTER = TIMER0_INITIAL + 2; // reset counter, but also add 2 since it takes 2 clock cycles to get going
 		// move counters, which is the job of this timer interrupt
-		delay_count++; // increment time delay
+		// delay_count++; // increment time delay
+		led_duty_cycle_counter++; // keep counting led
+		if (led_duty_cycle_counter >= led_duty_cycle)
+   		{
+   			if (led_duty_cycle_counter >= led_period)
+   			{
+   				led_duty_cycle_counter -= led_period; //reset led counter safely
+   				// led_state = ON; // we are in the ON part of the duty cycle
+   			}
+   			else
+   			{
+   				led_state = OFF;
+   			}
+   		}
+   		else
+   		{
+   			led_state = ON; // within On part of duty cycle
+   		}
+
+   		if (led_stay_on)
+   		{
+   			LED = ON;
+   		}
+   		else
+   		{
+   			LED = led_state;
+   		}
+
+   		// extra time stuff
+		if (button_bounce_count)
+		{
+			button_bounce_count--; // get closer to point in time that another button press can occur
+		}
+		if (ping_delay_count)
+		{
+			ping_delay_count--;
+		}
 	}
 	if (BUTTON_INTERRUPT_FLAG) // if the button has been pressed (Only IO Interrupt set)
 	{
@@ -68,14 +110,9 @@ void interrupt ISR()
 		if (!BUTTON && !button_bounce_count) // button was pressed and therefore this will read low (and we avoided bounce)
 		{
 			device_state = ~device_state; // toggle the stored button state so we can have an internal state based on it
-			button_bounce_count = button_bounce; // prevent this code from being triggered by the button bounce.
+			button_bounce_count = BUTTON_BOUNCE; // prevent this code from being triggered by the button bounce.
 		}
 	}  
-	// if (ADC_INTERRUPT_FLAG)
-	// {
-	// 	ADC_INTERRUPT_FLAG = CLEAR; // we are dealing with the interrupt
-	// 	led_duty_cycle = ADC_RESULT_HIGH; // set duty
-	// }
 }
 
 void runCalibration() //pull a threshold from the POT and set the DC bias of the receiver
@@ -119,7 +156,7 @@ void main()
 {	
 	CORE_CLOCK = 0x6B; // set the clock difference manually XXX change this if the chip changes
 	//set up ping
-	unsigned int ping_delay_count = ping_delay;
+	ping_delay_count = PING_DELAY;
 	T1_PIN1 = OUTPUT; // first transmit pin is output
 	T1_PIN2 = OUTPUT; // second transmit pin is output
 	RECEIVER_PIN = INPUT;
@@ -127,10 +164,7 @@ void main()
 
     // Set up timer0
     // calculate intial for accurate timing $ inital = TimerMax-((Delay*Fosc)/(Prescaler*4))
-    // This shrinks timing smaller than directly prescaling for when thats necessary
-    timer0_initial = 118; // the interrupt triggers every 256-this
-    timer0_delay = 1; // for 1ms and prescaler of 1:8 (adjusted empirically)
-   	TIMER0_COUNTER = timer0_initial; // set counter
+   	TIMER0_COUNTER = TIMER0_INITIAL; // set counter
    	TIMER0_CLOCK_SCOURCE = INTERNAL; // internal clock
    	PRESCALER = 0; // enable prescaler for Timer0
     PS2=0; PS1=1; PS0=0; // Set prescaler to 1:8
@@ -161,9 +195,6 @@ void main()
     ADC_INTERRUPT = OFF; // by default these aren't necessary
     ADC_ON = ON; // turn it on
 
-    //set up calc variables
-    unsigned long int magnitude1, magnitude2, magnitude3;
-    range_to_target.range = initial_range; // begin scanning at 61us which is approximately a 10mm difference
     
     //calibration
     TIMER1 = ON;
@@ -171,7 +202,12 @@ void main()
     TIMER1_INTERRUPT_FLAG = 0;
 
     runCalibration(); // pull a threshold from the POT and set the DC bias
-					    
+
+    //set up calc variables
+    magnitude1 = 0;
+    range_to_target.range = initial_range; // begin scan at an initial range, offset from 16bit max for use in the timer1
+    range_band = 20; // give 100 searches through search space to begin with 
+	
    	GLOBAL_INTERRUPTS = ON;
 
 
@@ -252,7 +288,7 @@ void main()
 
 
 	    	// clean up
-	    	ping_delay_count = ping_delay;
+	    	ping_delay_count = PING_DELAY;
 	    	GLOBAL_INTERRUPTS = ON; // turn these back on
 
 	    	//calculations can now happen
@@ -267,7 +303,7 @@ void main()
 			if (magnitude1 >= read_threshold) //  this is the rough beginning of the envelope of the wave, so we have found a distance
 			{
 				// led_duty_cycle = (range_to_target.range / 2)*10; // a rough output for verification purposes
-				if (range_to_target.range >= min_range)
+				if (range_to_target.range >= min_range) // check if the range (offset from the clock) is less than min range
 				{
 					led_stay_on = 1;
 					led_duty_cycle = 0;
@@ -277,82 +313,20 @@ void main()
 					led_stay_on = 0;
 					led_duty_cycle = rangeToDuty(range_to_target.range);
 				}
-				range_to_target.range = initial_range; // reset search
+				range_to_target.range = 0xFFFF - initial_range; // reset search
 			}
 			else //  still need to keep searching
 			{
 				range_to_target.range -= range_band; // search the next range band
 				
-				if (range_to_target.range <= max_range)
+				if (range_to_target.range <= max_range) //if we have gone beyond the limit we care about
 				{
 					range_to_target.range = initial_range; //reset the search to within 5cm
 					led_stay_on = 0;
 					led_duty_cycle = 0;
 				}
-			}
-
-	    	
-  		
+			}  		
     	}
-
-    	// State code
-    	// led with duty cycle
-    	if (delay_count >= timer0_delay) // runs approximately 1/1ms
-   		{
-   			delay_count -= timer0_delay; // reset counter safely
-   			led_duty_cycle_counter++; // increment the led counter
-   			if (button_bounce_count)
-			{
-				button_bounce_count--; // get closer to point in time that another button press can occur
-			}
-			if (ping_delay_count)
-			{
-				ping_delay_count--;
-			}
-
-   		}
-   		// TTT rewrite to ignore period, we need absolutes to obtain test values
-   		// if (led_duty_cycle_counter >= led_duty_cycle)
-   		// {
-   		// 	led_state = OFF;
-   		// }
-   		// else
-   		// {
-   		// 	led_state = ON;
-   		// }
-   		if (led_duty_cycle_counter >= led_duty_cycle)
-   		{
-   			if (led_duty_cycle_counter >= led_period)
-   			{
-   				led_duty_cycle_counter -= led_period; //reset led counter safely
-   				// led_state = ON; // we are in the ON part of the duty cycle
-   			}
-   			else
-   			{
-   				led_state = OFF;
-   			}
-   		}
-   		else
-   		{
-   			led_state = ON; // within On part of duty cycle
-   		}
-
-   		if (led_stay_on)
-   		{
-   			LED = ON;
-   		}
-   		else
-   		{
-   			LED = led_state;
-   		}
-
-   // 		//calculations
-   // 		if (do_calcs)
-  	// 	{
-  			
-
-			// do_calcs = OFF;
-  	// 	}
     }
     
     return;
